142d046dff0c2ed948145aa22a30984e6598c744
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 00:57:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length ==1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    array[0...-2].join(", ")+ ", " + array[-2..-1].join(", and ")
+  end
 end
